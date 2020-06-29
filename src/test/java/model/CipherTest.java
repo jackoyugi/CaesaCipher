adc262e30d1ec355927dd0c789a21cbe732e3c38
jackoyugi <oyugijack@gmail.com>
2020-06-29 15:30:18 +0300

    done small testing to CipherTest by changing assetEqual to assertTrue as a boolean

--- src/test/java/model/CipherTest.java
+++ src/test/java/model/CipherTest.java
@@ -5,29 +5,24 @@ import static org.junit.Assert.*;
 
 public class CipherTest {
     @Test
-    public void runCipher_instantiatesCorrectly() {
+    public void runCipher_instantiates() {
         Cipher testEncryption = new Cipher("z", 1);
-        assertEquals("true", testEncryption instanceof Cipher);
+        assertTrue("z", testEncryption instanceof Cipher);
     }
-//    @Test
-//    public void runCipher_getmInputString() {
-//        Cipher testEncryption = new Cipher("z", 1);
-//        assertEquals("z", testEncryption.getmInputString());
-//    }
-//    @Test
-//    public void runCipher_getmShift() {
-//        Cipher testEncryption = new Cipher("z", 1);
-//        assertEquals("1", testEncryption.getmShift());
-//    }
-//    @Test
-//    public void runCipher_encrypt() {
-//        Cipher testEncryption = new Cipher("z", 1);
-//        assertEquals("y", testEncryption.encrypt());
-//    }
-//    @Test
-//    public void runCipher_decrypt() {
-//        Cipher testEncryption = new Cipher("y", 1);
-//        assertEquals("z", testEncryption.decrypt());
-//    }
+    @Test
+    public void runCipher_getmInputString() {
+        Cipher testEncryption = new Cipher("z", 1);
+        assertEquals("z", testEncryption.getmInputString());
+    }
+    @Test
+    public void runCipher_encrypt() {
+        Cipher testEncryption = new Cipher("Hello", 4);
+        assertEquals("Lipps", testEncryption.encrypt());
+    }
+    @Test
+    public void runCipher_decrypt() {
+        Cipher testEncryption = new Cipher("Lipps", 4);
+        assertEquals("Hello", testEncryption.decrypt());
+    }
 
 }
